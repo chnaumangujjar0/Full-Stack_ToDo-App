6b779eccd9fd99ebe4a789e9bfc2ca574b5681fd
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-07-28 22:51:03 +0500

    feat: ensure upload directory exists before saving files in multer middleware

--- Todo-backend/src/middleware/multer.middleware.js
+++ Todo-backend/src/middleware/multer.middleware.js
@@ -4,7 +4,12 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    const uploadDir = "./public/temp";
+
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    cb(null, uploadDir)
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
